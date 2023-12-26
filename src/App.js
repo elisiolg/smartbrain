d97602a16f4b6f749c56378bfded47332bef6155
elisiolg <elisiolg@gmail.com>
2023-12-26 13:17:46 +0000

    Changes to be committed: modified:   src/App.js new file:   src/components/FaceRecognition/FaceRecognition.js modified:   src/components/ImageLinkForm/ImageLinkForm.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,99 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ParticlesBg from 'particles-bg'
+import { useState } from 'react';
+
+const returnClarifaiRequeestOptions = (ImageUrl) => {
+
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = '4e3c117bf8804c4db30667ef7699f64d';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'elisiolg';
+  const APP_ID = 'test';
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = ImageUrl;
+
+  const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+
+  return requestOptions
+}
 
 function App() {
+
+  const [input, setinput] = useState('');
+  // constructor = () => {
+  //   super();
+  //   this.state = {
+  //     input: '',
+  //   }
+  // }
+
+  var onInputChange = (e) => {
+    console.log(e.target.value)
+  }
+
+  let onButtonSubmit = () => {
+    console.log('click')
+
+    //app.models.predict('face-detection', input)
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiRequeestOptions)
+    .then(response => response.json())
+    .then(response => {
+      console.log('hi', response)
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count}))
+          })
+
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log(err));
+  }
+
   return (
     <div className="App">
       <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
+      <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit}/>
+      <FaceRecognition />
     </div>
   );
 }
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FaceRecognition = () => {
+    return (
+        <div className="center">
+            <img
+            alt=''
+            src="https://media.istockphoto.com/id/491520707/photo/sample-red-grunge-round-stamp-on-white-background.jpg?s=612x612&w=0&k=20&c=FW80kR5ilPkiJtXZEauGTghNBOgQviVPxAbhLWwnKZk=" />
+        </div>
+    )
+}
+
+export default FaceRecognition
