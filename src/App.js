ccb9027a062dbefbd9e01436dc0f3a6ff3405138
elisiolg <elisiolg@gmail.com>
2023-12-30 22:23:24 +0000

    Changes to be committed: modified:   src/App.js modified:   src/components/Rank/Rank.js modified:   src/components/Register/Register.js modified:   src/components/SignIn/SignIn.js

--- src/App.js
+++ src/App.js
@@ -9,50 +9,6 @@ import Signin from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import React, {Component} from 'react';
 
-const returnClarifaiRequeestOptions = (ImageUrl) => {
-  
-      // Your PAT (Personal Access Token) can be found in the portal under Authentification
-      const PAT = 'a629694dfab9427fa2f5a7c213dbce95';
-      // Specify the correct user_id/app_id pairings
-      // Since you're making inferences outside your app's scope
-      const USER_ID = 'elisiolg';       
-      const APP_ID = 'test';
-      // Change these to whatever model and image URL you want to use
-      const MODEL_ID = 'face-detection';
-      const IMAGE_URL = {ImageUrl};
-  
-      ///////////////////////////////////////////////////////////////////////////////////
-      // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-      ///////////////////////////////////////////////////////////////////////////////////
-  
-      const raw = JSON.stringify({
-          "user_app_id": {
-              "user_id": USER_ID,
-              "app_id": APP_ID
-          },
-          "inputs": [
-              {
-                  "data": {
-                      "image": {
-                          "url": IMAGE_URL
-                      }
-                  }
-              }
-          ]
-      });
-  
-      const requestOptions = {
-          method: 'POST',
-          headers: {
-              'Accept': 'application/json',
-              'Authorization': 'Key ' + PAT
-          },
-          body: raw
-      };
-
-      return requestOptions
-}
-
 class App extends Component {
   constructor() {
     super();
