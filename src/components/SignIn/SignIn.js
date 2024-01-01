20afd10c6cf553592009aba049f1d86b41716cbd
elisiolg <elisiolg@gmail.com>
2024-01-01 18:39:31 +0000

    Changes to be committed: modified:   src/App.js modified:   src/components/Register/Register.js modified:   src/components/SignIn/SignIn.js

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://smartbrain-api-qcx5.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
